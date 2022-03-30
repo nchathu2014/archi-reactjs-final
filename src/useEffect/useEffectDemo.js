99f7344fb795e6830cb5861c7acf9f12fc4a56f9
Chathuranga <nchathu2014@gmail.com>
2022-03-30 07:56:25 +0530

    Completed useEffect

--- src/useEffect/useEffectDemo.js
+++ src/useEffect/useEffectDemo.js
@@ -1,5 +1,30 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 export default function UseEffectDemo() {
-  return <div>useEffectDemo</div>;
+  const [users, setUsers] = useState(null);
+  const [counter, setCounter] = useState(0);
+
+  const handleOnClick = () => setCounter(counter + 1);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/comments")
+      .then((res) => {
+        console.log("API CALLED");
+        setUsers(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <>
+      <h2>Email App</h2>
+      <p>{users ? users[0].email : "no users"}</p>
+      <p>
+        <label>{counter}</label>
+      </p>
+      <button onClick={handleOnClick}>Click Me!</button>
+    </>
+  );
 }
